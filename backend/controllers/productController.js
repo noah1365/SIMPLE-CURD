3e241df8ce22386700ca453e351581b01e5e45aa
noah1365 <borlagdatanjohnlloyd1@gmail.com>
2024-08-20 19:08:06 +0800

    UPDATE PRODUCT

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -27,4 +27,18 @@ export const getProduct = async(req,res)=>{
     }
 }
 
+export const updateProduct = async(req,res)=>{
+    const {id} = req.params;
+    const product = req.body;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(400).json({success:false,message:"Invalid products ID"});
+    }
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id,product,{new:true});
+        res.status(200).json({success:true,data:updatedProduct})
+    } catch (error) {
+        res.status(500).json(({success:false,message:"Internal server Error"}));
+    }
+}
+
 
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createProduct, getProduct } from "../controllers/productController.js";
+import { createProduct, getProduct, updateProduct } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.post('/',createProduct);
 router.get('/',getProduct);
+router.put('/:id',updateProduct);
 
 export default router;
