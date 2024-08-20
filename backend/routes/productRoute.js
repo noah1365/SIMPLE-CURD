9d5b77b2847f967ba69942353f21a3bdf8bf24f2
noah1365 <borlagdatanjohnlloyd1@gmail.com>
2024-08-20 19:13:35 +0800

    DELETE PRODUCT

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createProduct, getProduct, updateProduct } from "../controllers/productController.js";
+import { createProduct, deleteProduct, getProduct, updateProduct } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.post('/',createProduct);
 router.get('/',getProduct);
 router.put('/:id',updateProduct);
+router.delete('/:id',deleteProduct);
 
 export default router;
